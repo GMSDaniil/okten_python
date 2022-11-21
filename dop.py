20a6f07d845cea01de173ed216ebe658c736877c
GMSDaniil <45737205+GMSDaniil@users.noreply.github.com>
2022-11-21 12:42:45 +0100

    Add files via upload

---
+++ dop.py
@@ -0,0 +1,43 @@
+data = [
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1111, "field": {}},
+        {"id": 1112, "field": {}},
+        {"id": 1113, "field": {}},
+        {"id": 1114, "field": {}},
+        {"id": 1115, "field": {}},
+    ],
+    [
+        {"id": 1110, "field": {}},
+        {"id": 1120, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1123, "field": {}},
+        {"id": 1124, "field": {}},
+        {"id": 1125, "field": {}},
+    ],
+    [
+        {"id": 1130, "field": {}},
+        {"id": 1131, "field": {}},
+        {"id": 1122, "field": {}},
+        {"id": 1132, "field": {}},
+        {"id": 1133, "field": {}},
+    ]
+]
+
+res = []
+x = 0
+while len(res) < 5:
+    for i in data:
+        for j in i:
+            if j['id'] not in res:
+                res.append(j['id'])
+                if len(res) == 5:
+                    x = 1
+                break
+        if x == 1:
+            break
+
+
+print(res)
+
+
